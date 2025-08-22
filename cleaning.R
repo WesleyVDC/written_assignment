36a0172e532015c7fa133e3e60c2dc410bb7847d
Taylor <taylor@acker.co.nz>
2025-08-23 09:43:01 +1200

    Completed file cleaning and setup participant folders.

--- cleaning.R
+++ cleaning.R
@@ -2,18 +2,25 @@
 library(tidyverse)
 library(stringr)
 
-file.rename("input/")
-
-list.files("input/")
-
+# Needed to link identifier in filename with student id
 participants <- read_csv("input/Grades-DATA201-DATA422-25S2-Written Advice on Māori Data Governance and Sovereignty-4325065.csv") %>%
   janitor::clean_names() %>%
   filter(status == "Submitted for grading -  -") %>%
   mutate(identifier = str_split_i(identifier, " ", 2)) %>%
   select(id_number, identifier)
-  
-paste0(
-  str_split_i(list.files("input/submissions/"), "_", 2),
-  ".",
-  str_split_i(list.files("input/submissions/"), "\\.", -1)
+
+# Warning - destructively renames files
+file.rename(
+  list.files("input/submissions/", full.names = TRUE),
+  paste0(
+    "input/submissions/",
+    str_split_i(list.files("input/submissions/"), "_", 2),
+    ".",
+    str_split_i(list.files("input/submissions/"), "\\.", -1)
+    )
 )
+
+# Create folder for each ID number
+sapply(participants$id_number, function(id) {
+  dir.create(file.path("allocation", id), showWarnings = FALSE)
+})
